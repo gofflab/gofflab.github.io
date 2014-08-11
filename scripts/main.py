f6d9c0829b1b9d2def2fc0c22d23e96c545eac25
Loyal Goff <loyalgoff@gmail.com>
2014-08-11 14:59:10 -0400

    Added old professional site outline

---
+++ scripts/main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+from Bio import Medline,Entrez
+import shutil
+from RNASeq.misc import pp
+from jinja2 import Environment,FileSystemLoader
+
+
+templateDir = "templates"
+env = Environment(loader=FileSystemLoader([templateDir]))
+
+##########################
+#Publication Information
+##########################
+
+pmIDs=['22991327',
+'22197703',
+'20348442',
+'19784364',
+'19257808',
+'18814314',
+'18657893',
+'18004940',
+'17916793',
+'17114923',
+'15469607',
+'23222703',
+'22383036',
+'21890647',
+'23401553',
+'23623381',
+'24107992',
+'24304912',
+'24381249',
+'24393486',
+'24463464'
+]
+
+pmIDs.sort(reverse=True)
+
+Entrez.email="loyalgoff@gmail.com"
+handle = Entrez.efetch(db="pubmed", id=pmIDs, rettype="medline",
+                           retmode="text")
+						   
+records = Medline.parse(handle)
+
+################
+#Pages
+################
+
+pages=[
+        ('/', 'index', '','Home'),
+        ('/', 'research', 'Experimental Biology','Research'),
+        ('/', 'publications', 'Experimental Biology', 'Publications'),
+        ('/', 'software', 'Computational Biology','Software'),
+        ('/', 'contact', 'General','Contact'),
+        ('/', 'about', 'General','About'),
+        ('/', 'links', 'General','Links')
+]
+
+#pp(list(records))
+
+def pubs():
+    template=env.get_template('pubs.html')
+    outHandle = open(outFile,'w')
+    print >>outHandle, template.render(records=list(records))
+
+def renderPage(pageName,**kwargs):
+    fname=pageName+'.html'
+    template=env.get_template(fname)
+    outHandle = open(fname,'w')
+    print >>outHandle, template.render(**kwargs)
+
+if __name__ == '__main__':
+  for page in pages:
+      try:
+          if page[1]=='publications':
+              renderPage(page[1],activePage=page[1],pages=pages,records=list(records))
+          else:
+              renderPage(page[1],activePage=page[1],pages=pages)
+      except:
+          shutil.copy(templateDir+"/min.template",templateDir+"/"+page[1]+".html")
+          renderPage(page[1],activePage=page[1],pages=pages)

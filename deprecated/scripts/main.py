ae59a7ed043db64c5b439ef2ae69ed25d5a8147e
Loyal Goff <loyalgoff@Loyals-MacBook-Pro.local>
2020-09-06 20:00:56 -0400

    directory reorg for new website

--- deprecated/scripts/main.py
+++ deprecated/scripts/main.py
@@ -4,8 +4,6 @@ from Bio import Medline,Entrez
 import shutil
 #from RNASeq.misc import pp
 from jinja2 import Environment,FileSystemLoader,exceptions
-import urllib.request as request
-import json
 
 
 templateDir = "templates"
@@ -14,7 +12,7 @@ env = Environment(loader=FileSystemLoader([templateDir]))
 ##########################
 #Publication Information
 ##########################
-### Fetch publications
+
 pmIDs=['22991327',
 '22197703',
 '20348442',
@@ -62,48 +60,12 @@ pmIDs=['22991327',
 
 pmIDs.sort(reverse=True)
 
-print(f"Fetching {len(pmIDs)} publication records from Entrez...")
 Entrez.email="loyalgoff@gmail.com"
 handle = Entrez.efetch(db="pubmed", id=pmIDs, rettype="medline",
                            retmode="text")
 
 records = Medline.parse(handle)
-print("\tDone")
 
-##########################
-#Preprint Information
-##########################
-### Fetch Preprints
-preprintIDs = [
-    '157149',
-    '148049',
-    '196394',
-    '196915',
-    '378950',
-    '395004',
-    '479287',
-    '447557',
-    '484410',
-    '726547',
-    '779694',
-    '2020.03.13.990549',
-    '2020.07.23.218586',
-    '2020.08.25.262832',
-]
-
-preprintIDs = reversed(preprintIDs)
-
-def fetchBioRxiv(preprintID):
-    biorxiv_api_url = f'https://api.biorxiv.org/details/biorxiv/10.1101/{preprintID}'
-    response = request.urlopen(biorxiv_api_url)
-    data = json.loads(response.read())
-    return(data)
-
-#print(fetchBioRxiv(preprintIDs[0]))
-print("Fetching Preprints from BioRxiv...")
-preprints = [fetchBioRxiv(id)['collection'][0] for id in preprintIDs]
-print(f"\t{len(preprints)} found")
-#print(preprints[-1]
 
 ################
 #Pages
@@ -111,21 +73,19 @@ print(f"\t{len(preprints)} found")
 
 pages=[
         ('/', 'index', '','Home'),
-        #('/', 'research', 'Experimental Biology','Research'),
+        ('/', 'research', 'Experimental Biology','Research'),
         ('/', 'people', 'General','People'),
         ('/', 'publications', 'Experimental Biology', 'Publications'),
-        ('/', 'preprints', 'Experimental Biology', 'Preprints'),
-        ('/', 'teaching', 'Teaching', 'Teaching'),
+        #('/', 'courses', 'Teaching', 'Courses'),
         ('/', 'software', 'Computational Biology','Software'),
-        ('/', 'datasets', 'Computational Biology', 'datasets'),
         ('/', 'contact', 'General','Contact'),
         #('/', 'about', 'General','About'),
         ('/', 'join', 'General','Join'),
         #('/', 'resources', 'General','Resources'),
-        #('/', 'links', 'General','Links'),
-        #('/', 'news', 'General', 'News')
-        ('/', 'blog', 'General', 'Blog'),
+        ('/', 'links', 'General','Links'),
+        ('/', 'news', 'General', 'News')
 ]
+
 
 #pp(list(records))
 
@@ -133,9 +93,6 @@ def pubs():
     template=env.get_template('pubs.html')
     outHandle = open(outFile,'w')
     print >>outHandle, template.render(records=list(records))
-
-#def preprints():
-#    template=env.get_template('preprints.html')
 
 def renderPage(pageName,**kwargs):
     fname=pageName+'.html'
@@ -149,11 +106,8 @@ if __name__ == '__main__':
       try:
           if page[1]=='publications':
               renderPage(page[1],activePage=page[1],pages=pages,records=list(records))
-          elif page[1]=='preprints':
-              renderPage(page[1],activePage=page[1],pages=pages,preprints=preprints)
           else:
               renderPage(page[1],activePage=page[1],pages=pages)
       except exceptions.TemplateNotFound:
-          print("No good template for %s" % (page[1]))
-          #shutil.copy(templateDir+"/min.template",templateDir+"/"+page[1]+".html")
-          #renderPage(page[1],activePage=page[1],pages=pages)
+          shutil.copy(templateDir+"/min.template",templateDir+"/"+page[1]+".html")
+          renderPage(page[1],activePage=page[1],pages=pages)
